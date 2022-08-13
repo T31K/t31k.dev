3e0b1696dc9023625e732931271ff6bc481c53ab
T31K <t31kmunwong@gmail.com>
2022-08-13 16:08:07 +0800

    feat(open): add dynamic medium followers count

---
+++ js/scrape.js
@@ -0,0 +1,12 @@
+$.getJSON(
+  "https://api.allorigins.win/get?url=" +
+    encodeURIComponent("https://t31k.medium.com/"),
+  function (data) {
+    let { contents } = data;
+    let followerNumber = contents.substring(
+      contents.indexOf("Followers") - 4,
+      contents.indexOf("Followers") - 1
+    );
+    $("#followerNumber").text(followerNumber);
+  }
+);
